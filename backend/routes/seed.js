843ae98c76a29f72047206d70e4594f494ddf4cf
Harshit Chauhan <140307979+harshit77453@users.noreply.github.com>
2025-01-11 17:53:44 +0530

    Update seed.js

--- backend/routes/seed.js
+++ backend/routes/seed.js
@@ -1,1 +1,7 @@
+const express = require('express');
+const { seedDatabase } = require('../controllers/seedController');
 
+const router = express.Router();
+router.get('/', seedDatabase);
+
+module.exports = router;
