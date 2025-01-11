5ad2f07a7cfc65886a0cc8f6e97b55e90b8b50b9
Harshit Chauhan <140307979+harshit77453@users.noreply.github.com>
2025-01-11 17:49:51 +0530

    Update transaction.js

--- backend/models/transaction.js
+++ backend/models/transaction.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    price: Number,
+    dateOfSale: Date,
+    sold: Boolean,
+    category: String
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
 
