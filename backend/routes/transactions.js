efa37559221e766e31861d355f3c1010ed92486d
Harshit Chauhan <140307979+harshit77453@users.noreply.github.com>
2025-01-11 17:54:21 +0530

    Update transactions.js

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const {
+    listTransactions,
+    getStatistics,
+    getBarChart,
+    getPieChart,
+    getCombinedData
+} = require('../controllers/transactionController');
+
+const router = express.Router();
+router.get('/list', listTransactions);
+router.get('/statistics', getStatistics);
+router.get('/barchart', getBarChart);
+router.get('/piechart', getPieChart);
+router.get('/combined', getCombinedData);
+
+module.exports = router;
 
